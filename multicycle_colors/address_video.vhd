74c32f988859a82e03d3826cefa6e232d81ccd5c
Gabriel Tutui <gabrielNT@live.com>
2014-12-11 22:05:49 -0200

    Alterações no address_video.vhd
    Foi diminuído o número de palavras exibidas na tela por linha de 10 para
    5, fazendo as alterações necessárias para isso funcionar corretamente.
    Além disso, corrigimos a maneira que a memória é representada no video,
    fazendo cada 4 bits numa palavra representarem um pixel(que vai ser
    replicado para o ajuste ao tamanho da tela).

--- multicycle_colors/address_video.vhd
+++ multicycle_colors/address_video.vhd
@@ -19,28 +19,41 @@ ARCHITECTURE behavior OF address_video IS
 SIGNAL column_std: std_LOGIC_VECTOR(9 downto 0);
 signal pixel: std_logic;
 signal row_div4 : unsigned(11 downto 0);
-signal pixel_r, pixel_g, pixel_b : std_logic;
+signal pixel_r, pixel_g, pixel_b, pixel_s : std_logic;
 
 BEGIN
 
   column_std <= std_logic_vector(to_unsigned(column, 10));
   row_div4 <= shift_right(to_unsigned(row, 12), 2); --divide os pixels para ficarem 4x4, um pixel representa 16
   video_address <= std_logic_vector(
-              (shift_left(row_div4, 3)) --10 words*2columns = 640 bits (cada word com 32 bits)
-            + (shift_left(row_div4, 1)) 
-            + (shift_right(to_unsigned(column, 12), 6)));  --divide por 64 para caber na tela
-  
+              (shift_left(row_div4, 2)) --10 words*2columns = 640 bits (cada word com 32 bits)
+            + (shift_left(row_div4, 0)) 
+            + (shift_right(to_unsigned(column, 12), 7)));  --divide por 64 para caber na tela
 
   -- Criando pixel_r , pixel_g , pixel_b para selecionar as cores 
   -- para testar e ver as cores possiveis
-  pixel_r <= not video_out(31-to_integer(unsigned(column_std(5 downto 1))));
-  pixel_g <= not video_out(30-to_integer(unsigned(column_std(5 downto 1))));
-  pixel_b <= not video_out(29-to_integer(unsigned(column_std(5 downto 1))));
+  pixel_r <= video_out(31-to_integer(unsigned(column_std(5 downto 1)))*4);
+  pixel_g <= video_out(30-to_integer(unsigned(column_std(5 downto 1)))*4);
+  pixel_b <= video_out(29-to_integer(unsigned(column_std(5 downto 1)))*4);
+  pixel_s <= video_out(28-to_integer(unsigned(column_std(5 downto 1)))*4);
 
   
   -- intensidade nas respectivas cores
-  VGA_R <=  (others=>pixel_r) when disp_ena='1' else (others => '0'); 
-  VGA_G <=  (others=>pixel_g) when disp_ena='1' else (others => '0');
-  VGA_B <=  (others=>pixel_b) when disp_ena='1' else (others => '0');
+  VGA_R(0) <= pixel_r when disp_ena='1' else '0'; 
+  VGA_G(0) <= pixel_g when disp_ena='1' else '0';
+  VGA_B(0) <= pixel_b when disp_ena='1' else '0';
+  
+  VGA_R(1) <= pixel_r when disp_ena='1' else '0'; 
+  VGA_G(1) <= pixel_g when disp_ena='1' else '0';
+  VGA_B(1) <= pixel_b when disp_ena='1' else '0';
+ 
+  VGA_R(2) <= pixel_r when disp_ena='1' else '0'; 
+  VGA_G(2) <= pixel_g when disp_ena='1' else '0';
+  VGA_B(2) <= pixel_b when disp_ena='1' else '0'; 
+  
+  VGA_R(3) <= pixel_s when disp_ena='1' else '0'; 
+  VGA_G(3) <= pixel_s when disp_ena='1' else '0';
+  VGA_B(3) <= pixel_s when disp_ena='1' else '0';
+ 
   
 END behavior;
